adbf118fe40b6fb6a3a2b998b9c4fe41e7108b96
Maxio <29357824+itsMaxio@users.noreply.github.com>
2024-01-13 00:17:51 +0100

    new start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,76 +1,88 @@
 #!/bin/bash
 
 printLog(){
+    echo -e "\n---------------------------------------------------------"
+    echo -e "[$1] $2"
     echo -e "---------------------------------------------------------"
-    echo -e "[SCRIPT] $1"
-    echo -e "---------------------------------------------------------\n"
 }
 
-echo -e "eula=true" > /server/eula.txt #root permission
+echo "eula=true" > /server/eula.txt #root permission
 
+TYPE=`echo "$TYPE" | tr '[:upper:]' '[:lower:]'`
+JAR_FILE="/server/server.jar"
+
+printLog "WELCOME" "Server informations"
+
+echo "JAVA_VERSION:    $JAVA_VERSION"
+echo "TYPE:            $TYPE"
+echo "LINK:            $LINK"
+echo "UID:             $UID"
+echo "GID:             $GID"
+echo "MIN_MEMORY:      $MIN_MEMORY"
+echo "MAX_MEMORY:      $MAX_MEMORY"
+# echo "MINCPU: "
+# echo "MAXCPU: "
+# echo "PORT: $PORT"
+
+
+printLog "USER" "Creating user and changing permissions"
+
+echo "Creating user: minecraft ($UID:$GID)"
 useradd -s /bin/bash minecraft > /dev/null 2>&1
 groupadd minecraft > /dev/null 2>&1
 
 groupmod -g $GID minecraft > /dev/null 2>&1
 usermod -u $UID minecraft > /dev/null 2>&1
 
+echo "Changing permissions"
 chown -R $UID:$GID /server > /dev/null 2>&1
 
-JAR_TYPE=`echo -e "$TYPE" | tr '[:upper:]' '[:lower:]'`
-JAR_FILE="/server/server.jar"
 
-echo -e "---------------------------------------------------------"
-echo -e "[SCRIPT] WELCOME..."
-echo -e "UID: $UID"
-echo -e "GID: $GID"
-echo -e "JAR_TYPE: $JAR_TYPE"
-echo -e "MIN_MEMORY: $MIN_MEMORY"
-echo -e "MAX_MEMORY: $MAX_MEMORY"
-echo -e "---------------------------------------------------------\n"
+printLog "START" "Starting as: $TYPE"
 
-if [[ $JAR_TYPE == "forge" ]] ; then 
+if [[ $TYPE == "forge" ]] ; then 
+
     if [[ ! -d "/server/libraries/net/minecraftforge/forge" ]] ; then
-        printLog "No libraries, trying to download"
-        
+        echo -e "No libraries, trying to install\n"
         gosu minecraft java -jar /server/forge.jar --installServer
     fi
 
     FORGE_VERSION=`ls /server/libraries/net/minecraftforge/forge`
-
-    printLog "Strating $JAR_TYPE"
     
+    echo -e "Install complete, staring server\n"
     exec gosu minecraft java -Xmx$MAX_MEMORY -Xms$MIN_MEMORY @/server/libraries/net/minecraftforge/forge/$FORGE_VERSION/unix_args.txt "$@" nogui
-elif [[ $JAR_TYPE == "file" ]] ; then
+
+elif [[ $TYPE == "file" ]] ; then
+
     if  [[ ! -f $JAR_FILE ]]; then
-        printLog "File does not exist... exiting!"
-
+        echo "File does not exist... exiting!"
         exit 1
     fi
 
-    printLog "File found, starting server"
-
+    echo -e "File found, starting server\n"
     exec gosu minecraft java -Xmx$MAX_MEMORY -Xms$MIN_MEMORY -jar /server/server.jar nogui
-elif [[ $JAR_TYPE == "link" ]] ; then
-    if [[ -z $LINK ]] ; then
-        printLog "No link url... exiting!"
 
+elif [[ $TYPE == "link" ]] ; then
+
+    if [[ -z $LINK ]] ; then
+        echo "No link url... exiting!"
         exit 1
     fi
 
-    printLog "Downloading server.jar from link"
-
+    echo "Downloading server.jar from link"
     curl -s -L "$LINK" --output /scripts/server.jar
 
     if [[ ! -f "/scripts/server.jar" ]] ; then
-        printLog "Something went wrong with file... Exiting!"
+        echo "Something went wrong with file... Exiting!"
         exit 1
     fi
 
-    printLog "Download complete, staring server"
-
+    echo -e "Download complete, staring server\n"
     exec gosu minecraft java -Xmx$MAX_MEMORY -Xms$MIN_MEMORY -jar /scripts/server.jar nogui
-else
-    printLog "Wrong type: $JAR_TYPE... exiting!"
 
+else
+
+    echo "Wrong type: $TYPE... exiting!"
     exit 1
+
 fi
